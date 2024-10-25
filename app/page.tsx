fe3c60da6d626704dc52a67fed9fb18b6bb11066
Jullyene18 <jullyene.ramos@wecode.digital>
2024-10-25 11:58:52 -0300

    setup inicial

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import { useEffect } from "react";
+
+export default function Home() {
+//  type testeTyping = {
+//   teste2:string|null
+//  }
+ 
+//   let teste:string
+
+//   useEffect(()=>{
+//     console.log('Hello Next.Js');
+//   },[])
+
+  return (
+<>a</>
+  );
+}
